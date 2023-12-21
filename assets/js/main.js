c953f1324e0740fe3bb224b061b65877c356e673
rukhsar624 <rukhsaranwar56@gmail.com>
2023-12-21 18:00:12 +0500

    add portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,87 @@
 import * as THREE from 'three';
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/controls/OrbitControls.js";
 // mouse cursor js open
-$(window).mousemove(function (e) {
-    $(".ring").css(
-        "transform",
-        `translateX(calc(${e.clientX}px - 1.25rem)) translateY(calc(${e.clientY}px - 1.25rem))`
-    );
+// $(window).mousemove(function (e) {
+//     $(".ring").css(
+//         "transform",
+//         `translateX(calc(${e.clientX}px - 1.25rem)) translateY(calc(${e.clientY}px - 1.25rem))`
+//     );
+// });
+// (() => {
+//     const hamburger = document.getElementById("hamburger");
+//     const menu = document.getElementById("overlay");
+//     let closed = true;
+  
+//     const change = () => {
+//       if (closed) {
+//         hamburger.classList.add("open");
+//         menu.classList.add("menu");
+//       } else {
+//         hamburger.classList.remove("open");
+//         menu.classList.remove("menu");
+//       }
+//       closed = !closed;
+//     };
+  
+//     hamburger.addEventListener("click", change);
+//   })();
+// scroll navbar
+$(window).scroll(function () {
+	var scroll = $(window).scrollTop();
+	if (scroll > 90) {
+		$("#navbar-fixed-top").css("background", "#000");
+		$("#navbar-fixed-top").css("box-shadow", "rgb(0 0 0 / 46%) 8px 8px 33px")
+	}
+	else {
+		$("#navbar-fixed-top").css("background", "none");
+		$("#navbar-fixed-top").css("box-shadow", "none")
+
+	}
+})
+  // MOBILE SCREEN SIDE NAV
+var sidebarBox = document.querySelector('#box');
+var sidebarBtn = document.querySelector('#btn');
+var pageWrapper = document.querySelector('#main-content');
+
+sidebarBtn.addEventListener('click', function (event) {
+
+	if (this.classList.contains('active')) {
+		this.classList.remove('active');
+		sidebarBox.classList.remove('active');
+	} else {
+		this.classList.add('active');
+		sidebarBox.classList.add('active');
+	}
 });
+
+pageWrapper.addEventListener('click', function (event) {
+
+	if (sidebarBox.classList.contains('active')) {
+		sidebarBtn.classList.remove('active');
+		sidebarBox.classList.remove('active');
+	}
+});
+window.addEventListener('keydown', function (event) {
+
+	if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
+		sidebarBtn.classList.remove('active');
+		sidebarBox.classList.remove('active');
+	}
+});
+
+
+function displayDiv(target){
+	
+	let display = document.getElementById(target).style.display;
+	if( display==='block'){
+		document.getElementById(target).style.display ='none';
+	}else{
+		document.getElementById(target).style.display ='block';
+		document.getElementById(target).style.display ='transform-active';
+	}
+	console.log(display);
+
+}
 //  mouse cursor js closed 
 const canvas = document.querySelector("#canvas");
 const leftZoomBtn = document.querySelector(".left-box-btn");
@@ -231,6 +306,8 @@ const animate = () => {
 };
 animate();
 // three js camera zoom into Image Closed
+
+
 
 
 
